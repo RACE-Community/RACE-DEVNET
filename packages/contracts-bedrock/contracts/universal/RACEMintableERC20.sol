d70089fdbcd42e7316167f9c11c988803d55ff42
L2nitant <131742613+L2nitant@users.noreply.github.com>
2023-06-07 19:28:21 +0530

    20.sol

--- packages/contracts-bedrock/contracts/universal/RACEMintableERC20.sol
+++ packages/contracts-bedrock/contracts/universal/RACEMintableERC20.sol
@@ -7,14 +7,9 @@ import { ILegacyMintableERC20, IOptimismMintableERC20 } from "./IOptimismMintabl
 import { Semver } from "../universal/Semver.sol";
 
 /**
- * @title OptimismMintableERC20
- * @notice OptimismMintableERC20 is a standard extension of the base ERC20 token contract designed
- *         to allow the StandardBridge contracts to mint and burn tokens. This makes it possible to
- *         use an OptimismMintablERC20 as the L2 representation of an L1 token, or vice-versa.
- *         Designed to be backwards compatible with the older StandardL2ERC20 token which was only
- *         meant for use on L2.
+
  */
-contract OptimismMintableERC20 is IOptimismMintableERC20, ILegacyMintableERC20, ERC20, Semver {
+contract RACEMintableERC20 is IRACEMintableERC20, ILegacyMintableERC20, ERC20, Semver {
     /**
      * @notice Address of the corresponding version of this token on the remote chain.
      */
